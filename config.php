9ae99bb742114d845b9781e74756206896976f75
ThiagoMarques <thiagossmarques@gmail.com>
2018-05-17 15:52:57 -0300

    DDL - Create, Drop, Alter

---
+++ config.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<!--
+DDL (DATA DEFINITION LANGUAGE) CREATE, DROP, ALTER
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h2>DDL - DATA DEFINITION LANGUAGE</h2>
+        <a href ="createdatabase.php"> 
+            <button>Criar Banco de Dados</button></a>
+        <a href="createtable.php"> 
+            <button>Criar Tabela</button></a>
+    </body>
+</html>
